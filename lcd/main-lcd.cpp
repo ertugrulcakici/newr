6e76ba9003ea6e4bd232bdc09bce338346ff6d7e
ertugrulcakici <ertugrul.cakicii@gmail.com>
2024-04-15 16:06:36 +0300

    .

--- lcd/main-lcd.cpp
+++ lcd/main-lcd.cpp
@@ -22,18 +22,21 @@ int main()
 {
 	lcd_init(); // LCD initialization
 
-	Circle circle = Circle({100, 100}, 70, {
-											   255,
-											   0,
-											   0,
-										   },
+	int semiHeight = LCD_HEIGHT / 2;
+	int semiWidth = LCD_WIDTH / 2;
+	int radius = semiHeight < semiWidth ? semiHeight : semiWidth;
+	Circle circle = Circle({semiWidth, semiHeight}, radius, {
+																255,
+																0,
+																0,
+															},
 						   {0, 0, 0});
 	circle.draw();
 
-	Character c = Character({0, 0}, 'A', {255, 255, 0}, {0, 0, 0});
-	c.draw();
-	Line topLeftToBottomRight = Line({0, 0}, {LCD_WIDTH, LCD_HEIGHT}, {255, 255, 255}, {0, 0, 0});
-	Line topRightToBottomLeft = Line({LCD_WIDTH, 0}, {0, LCD_HEIGHT}, {255, 255, 255}, {0, 0, 0});
-	topLeftToBottomRight.draw();
-	topRightToBottomLeft.draw();
+	// Character c = Character({0, 0}, 'A', {255, 255, 0}, {0, 0, 0});
+	// c.draw();
+	// Line topLeftToBottomRight = Line({0, 0}, {LCD_WIDTH, LCD_HEIGHT}, {255, 255, 255}, {0, 0, 0});
+	// Line topRightToBottomLeft = Line({LCD_WIDTH, 0}, {0, LCD_HEIGHT}, {255, 255, 255}, {0, 0, 0});
+	// topLeftToBottomRight.draw();
+	// topRightToBottomLeft.draw();
 }
