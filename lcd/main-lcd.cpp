8c8ffce79e9db12fb79811ee56ff480d7752596e
ertugrulcakici <ertugrul.cakicii@gmail.com>
2024-04-15 16:09:44 +0300

    .

--- lcd/main-lcd.cpp
+++ lcd/main-lcd.cpp
@@ -1,16 +1,3 @@
-// **************************************************************************
-//
-//               Demo program for labs
-//
-// Subject:      Computer Architectures and Parallel systems
-// Author:       Petr Olivka, petr.olivka@vsb.cz, 09/2019
-// Organization: Department of Computer Science, FEECS,
-//               VSB-Technical University of Ostrava, CZ
-//
-// File:         OpenCV simulator of LCD
-//
-// **************************************************************************
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -33,8 +20,8 @@ int main()
 						   {0, 0, 0});
 	circle.draw();
 
-	// Character c = Character({0, 0}, 'A', {255, 255, 0}, {0, 0, 0});
-	// c.draw();
+	Character c = Character({semiWidth, 0}, 'A', {255, 255, 0}, {0, 0, 0});
+	c.draw();
 	// Line topLeftToBottomRight = Line({0, 0}, {LCD_WIDTH, LCD_HEIGHT}, {255, 255, 255}, {0, 0, 0});
 	// Line topRightToBottomLeft = Line({LCD_WIDTH, 0}, {0, LCD_HEIGHT}, {255, 255, 255}, {0, 0, 0});
 	// topLeftToBottomRight.draw();
