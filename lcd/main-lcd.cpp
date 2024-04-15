f64a7e75d7a963cc3a47bc5e30fa653ad798711e
ertugrulcakici <ertugrul.cakicii@gmail.com>
2024-04-15 16:39:57 +0300

    .

--- lcd/main-lcd.cpp
+++ lcd/main-lcd.cpp
@@ -14,9 +14,9 @@
 int g_tab_sin[61] = {0, 104, 207, 309, 406, 499, 587, 669, 743, 809, 866, 913, 951, 978, 994, 1000, 994, 978, 951, 913, 866, 809, 743, 669, 587, 499, 406, 309, 207, 104, 0, -104, -207, -309, -406, -499, -587, -669, -743, -809, -866, -913, -951, -978, -994, -1000, -994, -978, -951, -913, -866, -809, -743, -669, -587, -499, -406, -309, -207, -104, 0};
 int g_tab_cos[61] = {1000, 994, 978, 951, 913, 866, 809, 743, 669, 587, 500, 406, 309, 207, 104, 0, -104, -207, -309, -406, -499, -587, -669, -743, -809, -866, -913, -951, -978, -994, -1000, -994, -978, -951, -913, -866, -809, -743, -669, -587, -500, -406, -309, -207, -104, 0, 104, 207, 309, 406, 500, 587, 669, 743, 809, 866, 913, 951, 978, 994, 1000};
 
-int g_angle = 0;
-RGB g_hand_color = {0, 255, 0}; // Green hand color
-int g_timer_state = 1;			// 1 for running, 0 for stopped
+// int g_angle = 0;
+// RGB g_hand_color = {0, 255, 0}; // Green hand color
+// int g_timer_state = 1;			// 1 for running, 0 for stopped
 
 void draw_clock_face()
 {
@@ -24,21 +24,21 @@ void draw_clock_face()
 	clock_face.draw();
 }
 
-void draw_hand()
-{
-	int hand_x = CLOCK_CENTER_X + HAND_LENGTH * g_tab_cos[g_angle] / 1000;
-	int hand_y = CLOCK_CENTER_Y + HAND_LENGTH * g_tab_sin[g_angle] / 1000;
-	Line hand({CLOCK_CENTER_X, CLOCK_CENTER_Y}, {hand_x, hand_y}, g_hand_color, {0, 0, 0});
-	hand.draw();
-}
+// void draw_hand()
+// {
+// 	int hand_x = CLOCK_CENTER_X + HAND_LENGTH * g_tab_cos[g_angle] / 1000;
+// 	int hand_y = CLOCK_CENTER_Y + HAND_LENGTH * g_tab_sin[g_angle] / 1000;
+// 	Line hand({CLOCK_CENTER_X, CLOCK_CENTER_Y}, {hand_x, hand_y}, g_hand_color, {0, 0, 0});
+// 	hand.draw();
+// }
 
-void update_timer()
-{
-	Line hand({CLOCK_CENTER_X, CLOCK_CENTER_Y}, {CLOCK_CENTER_X + HAND_LENGTH * g_tab_cos[g_angle] / 1000, CLOCK_CENTER_Y + HAND_LENGTH * g_tab_sin[g_angle] / 1000}, {0, 0, 0}, {0, 0, 0});
-	hand.hide();
-	g_angle = (g_angle + 6) % ANGLE_RANGE;
-	draw_hand();
-}
+// void update_timer()
+// {
+// 	Line hand({CLOCK_CENTER_X, CLOCK_CENTER_Y}, {CLOCK_CENTER_X + HAND_LENGTH * g_tab_cos[g_angle] / 1000, CLOCK_CENTER_Y + HAND_LENGTH * g_tab_sin[g_angle] / 1000}, {0, 0, 0}, {0, 0, 0});
+// 	hand.hide();
+// 	g_angle = (g_angle + 6) % ANGLE_RANGE;
+// 	draw_hand();
+// }
 
 void control_timer()
 {
@@ -56,9 +56,9 @@ int main()
 {
 	lcd_init();
 	draw_clock_face();
-	while (1)
-	{
-		control_timer();
-	}
+	// while (1)
+	// {
+	// 	// control_timer();
+	// }
 	return 0;
 }
